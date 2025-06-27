0208dae38cda3f41425f005a2d8e9da2e7c17c16
Vaibhav Karma <vaikarma@bestpeers.com>
2025-06-27 12:36:04 +0530

    fixed database

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_06_27_052036) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
