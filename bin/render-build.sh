ea9eda8278a959665f57a1443f68c3e215225dcc
Vaibhav Karma <vaikarma@bestpeers.com>
2025-06-27 16:09:39 +0530

    fixed render issue

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install Ruby dependencies
 bundle install
-yarn install
-bundle exec rake assets:precompile
+
+# Install JS dependencies using npm
+npm install
+
+# Precompile Vite assets
+bundle exec vite build
+
+# Run database migrations
 bundle exec rake db:migrate
